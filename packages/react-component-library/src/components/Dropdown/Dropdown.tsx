3e12d38457f34ee27c5901a5eb96c35d0bb64326
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-05-09 14:33:42 +0100

    fix(Dropdown): Correct box-shadow CSS rule

--- packages/react-component-library/src/components/Dropdown/Dropdown.tsx
+++ packages/react-component-library/src/components/Dropdown/Dropdown.tsx
@@ -9,7 +9,7 @@ import { DropdownLabel } from './DropdownLabel'
 import { DropdownOption } from './DropdownOption'
 import { DropdownPlaceholder } from './DropdownPlaceholder'
 
-const { color, spacing } = selectors
+const { color, shadow, spacing } = selectors
 
 export interface DropdownProps {
   /**
@@ -71,7 +71,7 @@ const StyledSelect = styled(Select)`
   }
 
   .rn-dropdown__menu {
-    @include m.shadow('1');
+    box-shadow: ${shadow('1')};
     top: 102%;
     margin: ${spacing('6')} 0 0 5%;
     width: 95%;
